7d5cb8715bde2d44368af5fc6c6a6002b8a00949
Alfrugo <adiez@mtm-inc.net>
2022-08-11 21:18:33 -0500

    got the camera movement imported from Blender

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { MixOperation } from 'three'
+import { CubeCamera, MixOperation } from 'three'
 
 import { TextureLoader } from 'three'
 
@@ -29,26 +29,86 @@ let mixer = null
 let cameraGLTF = null
 let controls = null
 
+/**
+ * Sizes
+ */
+ const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+/**
+ * Camera
+ */
+// Base camera
+let camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+let childCamera = null
+
 gltfLoader.load ('/models/AD/AD-logo.gltf', 
     (gltf) => 
     {   
         gltf.scene.traverse((child) =>
             {
+                // console.log(child.children[0].position)
                 child.material = bakedMaterial
-        
+
             })
         // animation of the elements in the scene
         mixer = new THREE.AnimationMixer(gltf.scene)
         const actionCamera = mixer.clipAction(gltf.animations[2])
         const actionAD = mixer.clipAction(gltf.animations[0])
-
+        
         actionCamera.play()
         actionAD.play()
+        
 
         scene.add(gltf.scene)
         // cameraGLTF = gltf.cameras [ 0 ]
         // scene.add(cameraGLTF)
         // cameraGLTF.scale.set (1,1,1)
+
+     
+        // camera.position.set(-3, 0.5, 5)
+        scene.add(camera)
+        
+        // console.log(gltf.scene.children.PerspectiveCamera)
+
+        const tick = () =>
+        {
+            const elapsedTime = clock.getElapsedTime()
+            const deltaTime = elapsedTime - previousTime
+            previousTime = elapsedTime
+        
+            // Mixer update animation for the AD logo
+            if (mixer !== null){
+                mixer.update(deltaTime)
+            }
+            // console.log(gltf.scene.children[0].position.x)
+
+            // if (cameraGLTF !== null){
+            //     // console.log (cameraGLTF)
+            //     renderer.render(scene, cameraGLTF)
+            //         // Update controls  donm't forget to un comment this part too when turning orbit controls on
+            //     controls.update() 
+            // }
+
+            renderer.render(scene, camera)
+            camera.position.set(gltf.scene.children[0].position.x, gltf.scene.children[0].position.y, gltf.scene.children[0].position.z)
+            // camera.lookAt(gltf.scene.children[1])
+        
+            
+        
+
+            //         // Update controls  donm't forget to un comment this part too when turning orbit controls on
+            // controls.update() 
+            window.requestAnimationFrame(tick)
+        
+            
+        
+        }
+        
+        tick()
+
+
 
     }
     );
@@ -87,13 +147,6 @@ gltfLoader.load ('/models/AD/AD-logo.gltf',
 // directionalLight.position.set(5, 5, 5)
 // scene.add(directionalLight)
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
 
 
 /**
@@ -115,18 +168,12 @@ bakedTexture.flipY = false
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
 
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(-3, 0.5, 5)
-scene.add(camera)
+
 
  // Controls   When making orbit actrive make sure to take out the comment section in the Tic animation section
- controls = new OrbitControls(camera, canvas)
- controls.target.set(0, 0.75, 0)
- controls.enableDamping = true
+//  controls = new OrbitControls(camera, canvas)
+//  controls.target.set(0, 0.75, 0)
+//  controls.enableDamping = true
 
 /**
  * Renderer
@@ -145,28 +192,3 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let previousTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
-
-    // Mixer update animation for the AD logo
-    if (mixer !== null){
-        mixer.update(deltaTime)
-    }
-        
-    // if (cameraGLTF !== null){
-    //     // console.log (cameraGLTF)
-    //     renderer.render(scene, cameraGLTF)
-    //         // Update controls  donm't forget to un comment this part too when turning orbit controls on
-    //     controls.update() 
-    // }
-
-    renderer.render(scene, camera)
-    //         // Update controls  donm't forget to un comment this part too when turning orbit controls on
-    controls.update() 
-    window.requestAnimationFrame(tick)
-}
-
-tick()
